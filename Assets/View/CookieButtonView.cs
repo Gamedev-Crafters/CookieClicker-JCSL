14b8c1269e824fc1a78106fbe26e8ce89272146a
PeliAzuh <kr2moran99@gmail.com>
2025-05-10 09:39:27 +0200

    asfasf

---
+++ Assets/View/CookieButtonView.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class CookieButtonView : MonoBehaviour, ICookieDisplay
+{
+    CookieStore cookieStore;
+    EarnCookies earnCookies;
+    TMP_Text  cookiesLabel;
+    
+    public int DisplayedCookies { get; }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       cookieStore = new CookieStore();
+       earnCookies = new EarnCookies(cookieStore, this);
+       cookiesLabel = gameObject.GetComponentInChildren<TMP_Text>();
+    }
+
+    public void ClickCookiButton()
+    {
+        earnCookies.Execute();
+        cookiesLabel.text = DisplayedCookies.ToString();
+    }
+
+}
